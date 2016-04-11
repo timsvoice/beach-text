a457bd923c502ef3394f9c8026c625b065e5fbc1
Timothy Voice <timsethvoice@gmail.com>
2016-04-11 17:14:11 -0400

    in process of reformatting data

--- imports/api/forecast/methods.js
+++ imports/api/forecast/methods.js
@@ -1,19 +1,36 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { _ } from 'underscore';
 
 import Forecast from 'forecast.io';
 
 const options = { APIKey: Meteor.settings.FORECAST_KEY };
-
 const forecast = new Forecast(options);
 
 export const getForecast = new ValidatedMethod({
   name: 'forecast.get',
   validate: null,
-  run({ lat, lng }) {
-    const forecastCall = Meteor.wrapAsync(forecast.get, forecast);
-    const forecastOptions = { exclude: 'minutely,daily,hourly,flags,alerts' };
-    const forecastResult = forecastCall(lat, lng, forecastOptions);
+  run({ lat, lng }) {    
+    let forecastCall = Meteor.wrapAsync(forecast.get, forecast);
+    let forecastOptions = { exclude:'minutely,hourly,flags,alerts' };
+    let forecastResult = forecastCall(lat, lng, forecastOptions).body;
     return forecastResult;
   },
 });
+
+export const getBeachForecast = new ValidatedMethod({
+  name: 'forecast.beach',
+  validate: null,
+  run({ lat, lng }) {
+    let forecastResult = getForecast.call({ lat, lng });
+    let beachForecast = {}
+
+    beachForecast.summary = forecastResult;
+  }
+})
+
+// getBeachForecast.call({
+//   lat: 10.345,
+//   lng: -10.56
+// })
